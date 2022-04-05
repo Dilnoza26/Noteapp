764b0c8b7462dbd795a332365ecc30f49b347530
Dilnoza26 <85575482+Dilnoza26@users.noreply.github.com>
2022-04-05 18:06:10 +0500

    hhhh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const PORT =process.env.PORT || 8001;
+const PORT =process.env.PORT || 8000;
 const mongoose = require("mongoose");
 const dotenv =  require('dotenv');
 dotenv.config()
